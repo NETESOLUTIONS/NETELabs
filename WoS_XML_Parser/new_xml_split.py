c2266b5f8e660f55aac5f05191bf3a2d55877cd8
George Chacko <chackoge@users.noreply.github.com>
2017-05-05 15:54:16 -0400

    Update new_xml_split.py

--- WoS_XML_Parser/new_xml_split.py
+++ WoS_XML_Parser/new_xml_split.py
@@ -1,14 +1,5 @@
-# Copyright 2017, NET ESOLUTIONS CORPORATION (NETE), McLean, VA.
-# Permission is hereby granted, free of charge, to any person obtaining a copy of this software and associated documentation files (the "Software"), to deal in the Software without 
-# restriction, including without limitation the rights to use, copy, modify, merge, publish, distribute, sublicense, and/or sell copies of the Software, and to permit persons to whom 
-# the Software is furnished to do so, subject to the following conditions:
-# The above copyright notice and this permission notice shall be included in all copies or substantial portions of the Software.
-# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE 
-# AND NONINFRINGEMENT. IN NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING 
-# FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
-
-# This script splits Web of Science XML files
-# Create Date: circa June 2016
+# This script splits Web of Science XML files and is used along with thr parser in this folder.
+# Creation Date: circa June 2016
 # Modified: 3/30/2017, Samet Keserci and George Chacko added comments and a README# 
 
 # 05/05/2017. While we have no record or notes of a fork, this new_xml_split.py script is an exact copy of
