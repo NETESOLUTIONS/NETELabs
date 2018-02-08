399ba9809f78f2bb48da0dc340864a9fd4574fc8
VJ Davey <akdavey@email.wm.edu>
2018-02-08 14:34:23 -0500

    update

--- PARDI_Dumps/derwent_merge.sh
+++ PARDI_Dumps/derwent_merge.sh
@@ -12,9 +12,9 @@ done
 # TODO: remove this and give our users the original file names after the next quarterly dump
 mv $merge_output/$prefix"_derwent_agents_update.csv" $merge_output/$prefix"_derwent_agent_update.csv"
 mv $merge_output/$prefix"_derwent_assignees_update.csv" $merge_output/$prefix"_derwent_assignee_update.csv"
-mv $merge_output/$prefix"_derwent_assignors_update.csv" $merge_output/$prefix"_derwent_assignor_update.csv"
 mv $merge_output/$prefix"_derwent_pat_citations_update.csv" $merge_output/$prefix"_derwent_citation_update.csv"
 mv $merge_output/$prefix"_derwent_examiners_update.csv" $merge_output/$prefix"_derwent_examiner_update.csv"
 mv $merge_output/$prefix"_derwent_inventors_update.csv" $merge_output/$prefix"_derwent_inventor_update.csv"
 mv $merge_output/$prefix"_derwent_lit_citations_update.csv" $merge_output/$prefix"_derwent_litcitation_update.csv"
 mv $merge_output/$prefix"_derwent_patents_update.csv" $merge_output/$prefix"_derwent_patent_update.csv"
+mv $merge_output/$prefix"_derwent_assignors_update.csv" $merge_output/$prefix"_derwent_assignor_update.csv"
