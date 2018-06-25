86100e8c79312a267d2b81f9e197b95b0d0d8a89
Dmitriy "DK" Korobskiy <dk@nete.com>
2018-06-25 11:44:50 -0400

    Clean previous dump archives

--- PARDI_Dumps/create_quarterly_dumps.sh
+++ PARDI_Dumps/create_quarterly_dumps.sh
@@ -1,33 +1,60 @@
+#!/usr/bin/env bash
 # VJ Davey
-# This script is used to create quarterly dumps of the postgres data for our end users
-# Usage:
-#       sh create_quarterly_dumps.sh baseline_dump_dir
-# where $baseline_dump_dir specifies the directory we want all the dump files to go to
 # Date 10/11/17
+
+if [[ $1 == "-h" ]]; then
+  cat <<'HEREDOC'
+NAME
+  create_quarterly_dumps.sh -- create quarterly export of the postgres data
+
+SYNOPSIS
+  create_quarterly_dumps.sh [output_directory]
+  create_quarterly_dumps.sh -h: display this help
+
+DESCRIPTION
+  Creates Postgres dump and CSV files for CG, CT, FDA, Derwent and WoS modules.
+  Archives weekly CSV exports for WoS and Derwent modules.
+  Uses the specified output_directory ({script_dir}/build/ by default).
+HEREDOC
+  exit 1
+fi
+
+set -ex
+set -o pipefail
+
+# Get a script directory, same as by $(dirname $0)
+script_dir=${0%/*}
+absolute_script_dir=$(cd "${script_dir}" && pwd)
+output_dir=${1:-${absolute_script_dir}/build} # $1 with the default
+if [[ ! -d "${output_dir}" ]]; then
+  mkdir "${output_dir}"
+  chmod g+w "${output_dir}"
+fi
+cd "${output_dir}"
+echo -e "\n## Running under ${USER}@${HOSTNAME} in ${PWD} ##\n"
 
 # Record start time
 echo "STARTED: $(date)"
 
-#output_dir
-output_dir=$1
-
 ## COMPILE PREVIOUS CSVs INTO A NEW QUARTERLY ARCHIVE
-cur_qtr=$(python -c "import datetime; import math; print \"%d_Q%d_\"%(2018,int(math.ceil(datetime.date.today().month/3.0)))")
+cur_qtr=$(python -c "import datetime; import math; print \"%d_Q%d\"%(2018,int(math.ceil(datetime.date.today().month/3.0)))")
 # Compile WOS records
 wos_weeklies=${output_dir}/weekly_updates/WOS
 wos_quarter_archive=${output_dir}/past_quarter_archive/WOS
-files="abstract address author dois grant keyword publication reference title"
-for file in $files; do
-  cat ${wos_weeklies}/*${file}_update.csv >> ${wos_quarter_archive}/${cur_qtr}_wos_${file}_archive.csv
+rm -f ${wos_quarter_archive}/${cur_qtr}_wos_*_archive.csv
+derwent_export_tables="abstract address author dois grant keyword publication reference title"
+for table in ${derwent_export_tables}; do
+  cat ${wos_weeklies}/*${table}_update.csv >> ${wos_quarter_archive}/${cur_qtr}_wos_${table}_archive.csv
 done
 #rm ${wos_weeklies}/*
 
 # Compile Derwent files
 derwent_weeklies=${output_dir}/weekly_updates/DERWENT
 derwent_quarter_archive=${output_dir}/past_quarter_archive/DERWENT
-files="agents assignees assignors pat_citations examiners inventors lit_citations patents"
-for file in $files; do
-  cat ${derwent_weeklies}/*${file}_update.csv >> ${derwent_quarter_archive}/${cur_qtr}_derwent_${file}_archive.csv
+rm -f ${derwent_quarter_archive}/${cur_qtr}_derwent_*_archive.csv
+derwent_export_tables="agents assignees assignors pat_citations examiners inventors lit_citations patents"
+for table in ${derwent_export_tables}; do
+  cat ${derwent_weeklies}/*${table}_update.csv >> ${derwent_quarter_archive}/${cur_qtr}_derwent_${table}_archive.csv
 done
 #rm ${derwent_weeklies}/*
 
@@ -41,7 +68,7 @@ exclude_string=""
 
 # Build the exclusion string (this sets exclusion options for the given tables and their sequences on the pg_dump command).
 for table in $exclude_tables ; do
-        exclude_string="${exclude_string} -T ${table} -T ${table}*_seq "
+  exclude_string="${exclude_string} -T ${table} -T ${table}*_seq "
 done
 
 # Build the different dump files
@@ -49,10 +76,11 @@ data_sources="cg ct fda derwent wos"
 for prefix in $data_sources; do
 
   # Generate pg_dump file
-  pg_dump pardi --section=pre-data --section=data --no-owner --no-privileges --no-tablespaces -t ${prefix}_* $exclude_string | gzip > ${baseline_dump_dir}/${prefix}_dump.sql.gz &
-  while [ "$(ps $! | grep $!)" ]; do
-    sleep 15; echo "still working to generate ${prefix} sql dump script ..."
-  done
+  pg_dump --section=pre-data --section=data --no-owner --no-privileges --no-tablespaces -t ${prefix}_* \
+          $exclude_string | gzip > ${baseline_dump_dir}/${prefix}_dump.sql.gz
+#  while [ "$(ps $! | grep $!)" ]; do
+#    sleep 15; echo "still working to generate ${prefix} sql dump script ..."
+#  done
   echo "FILE ${baseline_dump_dir}/${prefix}_dump.sql CREATED AND COMPRESSED: $(date)"
 
   # Generate CSV dump files
