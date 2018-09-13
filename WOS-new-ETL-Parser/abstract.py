639708ab8f218d7866a31e942a8fd0e45e9eecc6
Akshat Maltare <AKSHAT.MALTARE@GMAIL.COM>
2018-09-13 17:55:41 -0400

    Update abstract.py

--- WOS-new-ETL-Parser/abstract.py
+++ WOS-new-ETL-Parser/abstract.py
@@ -2,9 +2,9 @@
 
 '''
 Function:   this is a class file for document abstract data to save its value in the form of an object
+https://github.com/NETESOLUTIONS/NETELabs/blob/master/WOS-new-ETL-Parser/.readme
 
 
-USAGE:  	python abstract.py -filename file_name -csv_dir csv_file_directory
 Author: 	Akshat Maltare
 Date:		03/24/2018
 Changes:
